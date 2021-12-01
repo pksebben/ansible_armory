7cfc65faa876c057ca9c96b7f128d1d4bee32da5
pksebben <benmorsillo@gmail.com>
2021-12-01 14:10:36 -0800

    minor tweaks to order of operations

--- devkit/run.sh
+++ devkit/run.sh
@@ -6,7 +6,7 @@ RUNDIR="/home/coffee/tools/ansible_armory/devkit"
 # TODO: copy over pub key as well as private key
 # TODO: passwordless sudo - visudo (then, add:) 
 
-ansible-playbook -i "$RUNDIR/targets" "$RUNDIR/master_control.yml" 
+ansible-playbook -i "$RUNDIR/targets" "$RUNDIR/master_control.yml" -Kk
 
 # This is the necessary "first-run" version.  Use it once, and passwordless sudo as well as ssh should work
 # ansible-playbook -i "$RUNDIR/targets" "$RUNDIR/master_control.yml" -Kk
